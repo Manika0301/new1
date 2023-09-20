f785fdf10edf44575e57fb0b3c383138e72d4fa4
Manika0301 <104574888+Manika0301@users.noreply.github.com>
2023-09-20 10:24:11 +0530

    Add files via upload

---
+++ inter.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int main(){
+	float a[10][10],x,u1,u,y;
+	int i,j,n,n1,fact=1;
+	cout<<"Enter the n \n";
+	cin>>n;
+	cout<<"Enter the x:\n";
+	for(i=0;i<n;i++){
+	cin>>a[i][0];
+	}
+	cout<<"Enter the y:\n";
+	for(i=0;i<n;i++){
+	cin>>a[i][1];
+	}
+	cout<<"Enter the value to predict"<<endl;
+	cin>>x;
+	
+	for(j=2;j<n+1;j++){
+	for(i=0;i<n-j+1;i++)
+	a[i][j]=a[i+1][j-1]-a[i][j-1];
+	}
+	cout<<"The difference table is\n";
+	for(i=0;i<n;i++){
+	for(j=0;j<=n;j++)
+	cout<<"\t"<<a[i][j];
+	cout<<"\n";
+	}
+	y=a[n/2][1];
+	cout<<"\ny:"<<y;
+	u=(x-a[n/2][0])/(a[1][0]-a[0][0]);
+	u1=u;
+	cout<<"\n"<<u1;
+	for(i=2;i<n;i++)
+	{
+	 y=y+(u1/fact)*a[(n-1)/i][i];
+	 cout<<"\t"<<a[(n-1)/i][i];
+	 fact=fact*i;
+	 if(i==2){
+	 	u1=u1*(u-1);
+		}
+	 else{
+	    u1=u1*(u-(i-2));
+	}
+	}
+
+cout<<"\n\n The desired value is"<<y<<endl;
+return 0;
+}
+
+	 
